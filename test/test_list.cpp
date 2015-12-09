5da3d09be73d19801b7302123e97873d7418b61f
Alexander Khlybov <alekshlybov23@gmail.com>
2015-12-09 11:24:21 +0300

    Initial tests.

--- test/test_list.cpp
+++ test/test_list.cpp
@@ -1,2 +1,11 @@
+#include <gtest.h>
+
 #include "array_list.h"
 
+TEST(List, can_create_list){
+	ASSERT_NO_THROW(List list(3));
+}
+
+TEST(List, throws_when_create_list_with_negative_size_of_buffer_memory){
+	ASSERT_ANY_THROW(List list(-1));
+}
